a91ddc3e6e11fc1e63232a9956173f338f2da3ae
hansol <yunh1999@naver.com>
2024-07-14 16:37:39 +0900

    Breakingupdate: origin/1-보드-생성-api만들기

--- src/modules/board/board.controller.ts
+++ src/modules/board/board.controller.ts
@@ -1,8 +1,21 @@
-import { Controller } from '@nestjs/common';
-import { BoardService } from './board.service';
+import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
 
-//Dto tile
+import { BoardEntity } from "../../entities/board.entity"; 
 
+import { GenerateBoardDto } from "./dto/board.generate.dto";
+import { BoardService } from "./board.service";
 
-@Controller('board')
-export class BoardController {}
+@Controller("board")
+export class BoardController {
+  constructor(private readonly boardService: BoardService) {}
+
+  //Board 생성
+  @Post("v1/boards")
+  async generateBoard(@Body() generateBoardDto: GenerateBoardDto) {
+    return await this.boardService.generateBoard(
+      generateBoardDto.title,
+      generateBoardDto.content,
+      generateBoardDto.color,
+    );
+  }
+}
