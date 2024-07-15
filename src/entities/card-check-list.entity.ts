1f615ee250a4a2a88ccdef2524bbb2b2b252e9d9
김만규 <123226237+fierceCry@users.noreply.github.com>
2024-07-15 15:20:28 +0900

    feat: card-checklist 엔터티 추가 및 cards 엔터티와 연관 설정
        - card-checklist.entity.ts에 CardCheckListEntity 정의
        - typeorm.config.ts 업데이트하여 CardCheckListEntity 포함
        - CardsEntity 수정하여 CardCheckListEntity와 관계 설정

---
+++ src/entities/card-check-list.entity.ts
@@ -0,0 +1,26 @@
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { CardsEntity } from "./cards.entity";
+
+@Entity('card-check-list')
+export class CardCheckListEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({name: 'card_id'})
+  cardId:number;
+
+  @Column({ name: "check_comment" })
+  checkComment: string;
+
+  @Column({ type: "boolean",default: false, name: "is_done" })
+  isDone: boolean;
+
+  @CreateDateColumn({ name: "created_at" })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: "updated_at" })
+  updatedAt: Date;
+
+  @ManyToOne(() => CardsEntity, (card) => card.checklists)
+  card: CardsEntity;
+}
