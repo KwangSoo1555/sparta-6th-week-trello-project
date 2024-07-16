dec0613aed360c5c53eb96d18e0da18f40a1a7a9
clearghost3 <165910897+clearghost3@users.noreply.github.com>
2024-07-16 16:54:03 +0900

    create: redisFileFramed

---
+++ src/common/constants/env.constant.ts
@@ -0,0 +1,6 @@
+export const ENV = {
+  REDIS_HOST: process.env.REDIS_HOST,
+  REDIS_PORT: process.env.REDIS_PORT,
+  REDIS_USERNAME: process.env.REDIS_USERNAME,
+  REDIS_PASSWORD: process.env.REDIS_PASSWORD,
+};
