9b795076a7cde36d2ee396a26d21a235cfeddbd8
songsamuel <songsme2886@gmail.com>
2024-07-16 17:17:22 +0900

    엔티티 수정

--- src/entities/cards.entity.ts
+++ src/entities/cards.entity.ts
@@ -2,6 +2,7 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  JoinColumn,
   ManyToOne,
   OneToMany,
   PrimaryGeneratedColumn,
@@ -45,6 +46,7 @@ export class CardsEntity {
   updatedAt: Date;
 
   @ManyToOne(() => ListsEntity, (list) => list.card)
+  @JoinColumn({ name: "list_id" })
   list: ListsEntity;
 
   @OneToMany(() => FilesEntity, (file) => file.card)
@@ -59,6 +61,6 @@ export class CardsEntity {
   @OneToMany(() => CardCheckListEntity, (checklist) => checklist.card)
   checklists: CardCheckListEntity[];
 
-  @OneToMany(() => NotificationEntity, (notification)=> notification.card)
+  @OneToMany(() => NotificationEntity, (notification) => notification.card)
   notification: NotificationEntity[];
 }
